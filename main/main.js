965e6a7ede7067c8ec51fad9edfe219e4d56f121
Benjamin Green <blg40@students.uwf.edu>
2024-04-07 01:36:44 -0500

    testing cdn import

--- main/main.js
+++ main/main.js
@@ -1,45 +1,27 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
+
 const controls = new OrbitControls( camera, renderer.domElement );
+
+//controls.update() must be called after any manual changes to the camera's transform
 camera.position.set( 0, 20, 100 );
 controls.update();
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, cubeMaterial );
-scene.add( cube );
-
-const lineMaterial = new THREE.LineBasicMaterial( { color: 0x0000f} );
-
-const points = [];
-points.push ( THREE.Vector3( -10, 0, 0) );
-points.push( new THREE.Vector3( 0, 0, 0) );
-points.push( new THREE.Vector3( 10, 0, 0) );
-
-const pointGeomtry = new THREE.BufferGeometry().setFromPoints( points );
-const line = new THREE.Line( geometry, material );
-
-camera.position.z = 5;
-
 function animate() {
+
 	requestAnimationFrame( animate );
+
+	// required if controls.enableDamping or controls.autoRotate are set to true
 	controls.update();
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
 	renderer.render( scene, camera );
+
 }
-
-scene.add( line );
-renderer.renderer( scene, camera );
-
-animate();
